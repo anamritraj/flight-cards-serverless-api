de3c82576b6c4d0296fff2defa6fe9c929550089
anamritraj <anandamritraj904@gmail.com>
2018-04-19 23:48:20 +0530

    Add robust error handling and session management.

--- handlers/mainHandler.js
+++ handlers/mainHandler.js
@@ -3,10 +3,12 @@ const rp = require('request-promise');
 const flightFacts = require('../resources/flight-facts');
 const mainHandler = {
     'LaunchRequest': function () {
-        this.response.cardRenderer("Hello! Welcome to Flight Cards!");
-        this.response.speak("Hello! Welcome to Flight Cards!");
+        this.response.cardRenderer("Hello! Welcome to Flight Cards! You can ask me for cheapest flights from one place to another on any date or ask me for a flight fact or ask me if a place has an Airport. So what can I do for you?");
+        this.response.speak("Hello! Welcome to Flight Cards! You can ask me for cheapest flights from one place to another on any date, or ask me for a flight fact, or ask me if a place has an Airport. So what can I do for you?");
+        this.response.listen("Please say that again?");
         this.emit(':responseReady');
     },
+
     'GetFlightPriceIntent': function () {
     	let date = this.event.request.intent.slots.date.value,
     		passengers = this.event.request.intent.slots.passengers.value,
@@ -14,10 +16,17 @@ const mainHandler = {
     		src = this.event.request.intent.slots.src.value,
     		classType = "E",
     		src_code = undefined,
-    		dest_code = undefined;
+    		dest_code = undefined,
+    		SSML_RESPONSE_TEXT = "",
     		RESPONSE_TEXT = "",
     		RANDOM_FAIL = getRandomSpeechcon(failSpeechcons);
-
+    		if(!date){
+				RESPONSE_TEXT = getRandomSpeechcon(failSpeechcons) + "! You should specify a date for when you want to book a flight!"
+				this.response.cardRenderer(RESPONSE_TEXT);
+				this.response.speak(RESPONSE_TEXT);
+	        	this.response.listen("Please say that again?");
+				this.emit(':responseReady');
+			}
 		    let dateObj = new Date(date);
 		    let currentDateObj = new Date();
 
@@ -29,9 +38,11 @@ const mainHandler = {
 
 		    if (dateObj.getTime() < currentDateObj.getTime()) {
 		    	// Date is in past
-		    	RESPONSE_TEXT = "<say-as interpret-as=\"interjection\">"+ RANDOM_FAIL +"</say-as>! The date should be today's date or a date in future. Please try again.";
-		    	this.response.cardRenderer(""+ RANDOM_FAIL +"! The date should be today's date or a date in future. Please try again.");
-		        this.response.speak(RESPONSE_TEXT);
+		    	SSML_RESPONSE_TEXT = "<say-as interpret-as=\"interjection\">"+ RANDOM_FAIL +"</say-as>! The date should be today's date or a date in future. Please try again.";
+		    	RESPONSE_TEXT = ""+ RANDOM_FAIL +"! The date should be today's date or a date in future. Please try again.";
+		    	this.response.cardRenderer(RESPONSE_TEXT);
+		        this.response.speak(SSML_RESPONSE_TEXT);
+		        this.response.listen("Please say that again?");
 		        this.emit(':responseReady');
 		    }
 
@@ -39,7 +50,11 @@ const mainHandler = {
 		    let day = '' + dateObj.getDate();
 		    let year = '' + dateObj.getFullYear();
 		    let dateNew =   day + "/" + month + "/" + year;
-
+		    let SSML_DATE = "";
+		    if(year > currentYear)
+	    		SSML_DATE = "<say-as interpret-as=\"date\" >"+dateNew+"</say-as>";
+		    else
+				SSML_DATE = "<say-as interpret-as=\"date\" format=\"dm\">"+day+"/"+month+"</say-as>";
     	// Get place codes
     	for (let i = config.CITIES.length - 1; i >= 0; i--) {
     		if(config.CITIES[i].city.toLowerCase() == dest.toLowerCase()){
@@ -59,10 +74,11 @@ const mainHandler = {
     	if (!src_code || !dest_code) {
 			this.response.cardRenderer("Sorry! we dont know that place, can you please try again?");
 	        this.response.speak("Sorry! we dont know that place, can you please try again?");
+	        this.response.listen("Please say that again?");
 	        this.emit(':responseReady');
     	}
+
     	// Conslole Logs
-    	
         console.log("Date", dateNew);
         console.log("passengers", this.event.request.intent.slots.passengers);
         console.log("dest", this.event.request.intent.slots.dest);
@@ -82,48 +98,64 @@ const mainHandler = {
 			"&noOfInfnt=0"+
 			"&tripType=O"+
 			"&tripTypeDup=O";
+
 		console.log(URL);
+		
 		rp(URL)
 			.then(result => {
 				console.log(result);
 				result = JSON.parse(result);
 				if (result.flights === undefined) {
 					// there are no flights
-					RESPONSE_TEXT = "<say-as interpret-as=\"interjection\">"+ RANDOM_FAIL +"</say-as>! There seems to be no flights for this route and date. Please try again with a different date.";
-			    	this.response.cardRenderer(""+ RANDOM_FAIL +"!  There seems to be no flights for this route and date. Please try again with a different date.");
+					SSML_RESPONSE_TEXT = "<say-as interpret-as=\"interjection\">"+ RANDOM_FAIL +"</say-as>! There seems to be no flights for this route and date. Please try again with a different date.";
+					RESPONSE_TEXT = ""+ RANDOM_FAIL +"!  There seems to be no flights for this route and date. Please try again with a different date.";
+			    	this.response.cardRenderer(SSML_RESPONSE_TEXT);
 			        this.response.speak(RESPONSE_TEXT);
+			        this.response.listen("Please say that again?");
 			        this.emit(':responseReady');
 				}
+
 				let flight_class = result.flights["0"].le["0"].cls;
 				let flight_alirlines = result.flights["0"].le["0"].an;
 				let flight_code = result.flights["0"].le["0"].cc;
 				let flight_number = result.flights["0"].le["0"].fn;
-				
 				let departure_date = result.flights["0"].le["0"].fmtDeparture;
 				let departure_time = result.flights["0"].le["0"].fmtDepartureTime;
 				let arrival_date = result.flights["0"].le["0"].fmtArrival;
 				let arrival_time = result.flights["0"].le["0"].fmtArrivalTime;
 				let stops = (result.flights["0"].le["0"].noOfStops == "Non Stop") ?  "is "+result.flights["0"].le["0"].noOfStops : "has "+result.flights["0"].le["0"].noOfStops;
 				let fare = result.flights["0"].af;
+
 				passengers = (passengers == 1) ? passengers+' person': passengers+' persons';
-				RESPONSE_TEXT = "The cheapest flight from "+src+" to "+ dest + " for "+ passengers + " is "+flight_alirlines+ " "+flight_code +" <say-as interpret-as=\"digits\">"+flight_number+"</say-as>, which costs "+ fare+ " Rupees per person for "+flight_class+" class, and " + stops +".";
+
+				SSML_RESPONSE_TEXT = "The cheapest flight on "+SSML_DATE+" from "+src+" to "+ dest + " for "+ passengers + " is "+flight_alirlines+ " "+flight_code +" <say-as interpret-as=\"digits\">"+flight_number+"</say-as>, which costs "+ fare+ " Rupees per person for "+flight_class+" class, and " + stops +".";
+				RESPONSE_TEXT = "The cheapest flight on "+ dateNew +" from "+src+" to "+ dest + " for "+ passengers + " is "+flight_alirlines+ " "+flight_code +" "+flight_number+", which costs "+ fare+ " Rupees per person for "+flight_class+" class, and " + stops +".";
 				//"It departs from "+ src + " at " + departure_time + ", " + departure_date + " and arrives to " + dest + " at " + arrival_time + ", "+ arrival_date + "."; 
-				console.log(RESPONSE_TEXT);
+				
+				console.log(SSML_RESPONSE_TEXT);
 				this.response.cardRenderer(RESPONSE_TEXT);
-				this.response.speak(RESPONSE_TEXT);
+				this.response.speak(SSML_RESPONSE_TEXT);
         		this.emit(':responseReady');
 		    })
 		    .catch(err => {
 		    	console.log(err);
 				this.response.cardRenderer("There was an error. Please try again!");
 				this.response.speak("There was an error. Please try again!");
+		        this.response.listen("Please say that again?");
         		this.emit(':responseReady');
 		    });
     },
 
     'CheckAirportIndiaIntent': function(){
     	let place = this.event.request.intent.slots.place.value;
-		console.log(place);
+		if(!place){
+			RESPONSE_TEXT = getRandomSpeechcon(failSpeechcons) + "! You should specify a place where you want to check for an Airport!"
+			this.response.cardRenderer(RESPONSE_TEXT);
+			this.response.speak(RESPONSE_TEXT);
+        	this.response.listen("Please say that again?");
+			this.emit(':responseReady');
+		}
+		console.log("CheckAirportIndiaIntent: Requested for "+ place);
 		var result = checkAirport(place),
 			RESPONSE_TEXT;
 		if (result) {
@@ -133,18 +165,31 @@ const mainHandler = {
 		}
 		this.response.cardRenderer(RESPONSE_TEXT);
 		this.response.speak(RESPONSE_TEXT);
-		this.emit(':responseReady');
-    },
-    
-    'GetFlightFactIntent': function(){
-    	let RESPONSE_TEXT = "Here's a random flight fact for you: "+flightFacts.getRandomFact()
-		this.response.cardRenderer(RESPONSE_TEXT);
-		this.response.speak(RESPONSE_TEXT);
+		this.response.shouldEndSession(false)
 		this.emit(':responseReady');
     },
 
-    'Unhandled': function () {
-        this.emit(':ask', "Sorry I didnt understand that. Say help for assistance.");
+    'GetFlightFactIntent': function(){
+    	let RESPONSE_TEXT = "Here's a random flight fact for you: "+flightFacts.getRandomFact()
+    	console.log(RESPONSE_TEXT);
+		this.response.cardRenderer(RESPONSE_TEXT);
+		this.response.speak(RESPONSE_TEXT);
+		this.response.shouldEndSession(false);
+		this.emit(':responseReady');
+    },
+
+    'CustomHelpIntent': function(){
+    	let RESPONSE_TEXT = "You can ask me for cheapest flights from one place to another on any date, or ask me for a flight fact, or ask me if a place has an Airport. So what can I do for you?"
+		this.response.cardRenderer(RESPONSE_TEXT);
+		this.response.speak(RESPONSE_TEXT);
+        this.response.listen("Please say that again?");
+		this.emit(':responseReady');
+    },
+
+    'Unhandled': function () {;
+		this.response.speak("Sorry I didnt understand that. Say help if you need any assistance.");
+        this.response.listen("Please say that again?");
+		this.emit(':responseReady');
     }
 };
 
